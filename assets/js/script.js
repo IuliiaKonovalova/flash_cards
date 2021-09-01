153130fd73cfbafa1f657c5cc345c6baf993f4ee
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-09-01 19:45:08 +0800

    Style flash card, add flip effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,5 @@
 const card = document.querySelector('.card');
-console.log(card);
 
 card.addEventListener('click', function (e) {
-     card.classList.add('is-flipped');
+     card.classList.toggle('is-flipped');
 });
-
-console.log('hello');
-
-// function cardFlip() {
-//      if (card.classList.contains('card--front')) {
-//           card.classList.replace('card--front', 'card--back');
-//      }
-//      if (card.classList.contains('card--back')) {
-//           card.classList.replace('card--back', 'card--front');
-//      }
-
-// }
-
-// card.addEventListener('click', cardFlip);
