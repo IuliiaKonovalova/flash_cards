a23effb6f41aa34a43afa932e552ab432c05866d
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-09-02 15:06:05 +0800

    Add images for the FC

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
 const card = document.querySelector('.card');
 
 card.addEventListener('click', function (e) {
+     e.preventDefault();
      card.classList.toggle('is-flipped');
 });
+
+const flashcards = [
+     { image: img, word: banana, ipa: banana },
+     { image: img, word: banana, ipa: banana },
+     { image: img, word: banana, ipa: banana },
+     { image: img, word: banana, ipa: banana },
+     { image: img, word: banana, ipa: banana },
+     { image: img, word: banana, ipa: banana },
+     { image: img, word: banana, ipa: banana },
+];
