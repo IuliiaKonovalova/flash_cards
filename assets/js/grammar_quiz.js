4511cf35f20e041a6c90e604a3413d116bd53b76
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-09-04 20:31:43 +0800

    Style grammar quiz page

--- assets/js/grammar_quiz.js
+++ assets/js/grammar_quiz.js
@@ -1,11 +1,14 @@
 /// << << GRAMMAR QUIZ >> >> ///
 
 const cardNextButton = document.querySelector('#card__next');
+const answerOptions = document.getElementById('grammar-quiz__answer');
 
 // Wait for the DOM to finish loading before running the quiz
 
 document.addEventListener('DOMContentLoaded', function () {
      generateGrammarQuestion();
+
+     answerOptions.addEventListener('click', checkForAnswer);
 
      // Change Card on click
 
@@ -71,6 +74,34 @@ const grammarQuestions = [
      },
 ];
 
+// let shuffledQuestions = []; //empty array to hold shuffled selected questions out of all available questions
+
+// function handleQuestions() {
+//      //function to shuffle and push 10 questions to shuffledQuestions array
+//      //app would be dealing with 10questions per session
+//      while (shuffledQuestions.length <= 10) {
+//           const createRandomBlock = function () {
+//                return Math.floor(Math.random() * grammarQuestions.length);
+//           };
+//           if (!shuffledQuestions.includes(createRandomBlock)) {
+//                shuffledQuestions.push(createRandomBlock);
+//           }
+//      }
+// }
+
+// let questionNumber = 1; //holds the current question number
+// let playerScore = 0; //holds the player score
+// let wrongAttempt = 0; //amount of wrong answers picked by player
+// let indexNumber = 0; //will be used in displaying next question
+
+// function NextQuestion(index) {
+//      handleQuestions();
+//      const currentQuestion = shuffledQuestions[index];
+//      document.getElementById('grammar-quiz__score').innerHTML = playerScore;
+//      document.getElementById('grammar-quiz__question-number').innerHTML =
+//           currentQuestion.question;
+// }
+
 /**
  *
  * Generate random index for the Quiz
@@ -85,7 +116,7 @@ const createRandomBlock = function () {
  * with all visible elements
  */
 
-const generateGrammarQuestion = function (question) {
+const generateGrammarQuestion = function () {
      let grammarQuestion = document.getElementsByClassName(
           'grammar-quiz__question'
      )[0];
@@ -103,6 +134,8 @@ const generateGrammarQuestion = function (question) {
 
      let randomBlockIndex = createRandomBlock();
 
+     //Add data to the html from the grammarQuestions array
+
      grammarQuestion.innerText =
           grammarQuestions[randomBlockIndex].grammarQuestion;
 
@@ -110,11 +143,65 @@ const generateGrammarQuestion = function (question) {
           grammarAnswers[i].innerText =
                grammarQuestions[randomBlockIndex].grammarChoices[i].text;
      }
-
-     // for (answer of grammarAnswers) {
-     //      answer.innerText =
-     //           grammarQuestions[randomBlockIndex].grammarChoices[0].text;
-
-     //      grammarQuestions[randomBlockIndex].grammarChoices.shift();
-     //  }
 };
+
+function checkForAnswer() {
+     if (
+          grammarQuestions[randomBlockIndex].grammarChoices[i].correct === true
+     ) {
+          document.getElementsByClassName(
+               'grammar-quiz__answer'
+          ).style.backgroundColor = 'red';
+     }
+}
+//      const currentQuestion = shuffledQuestions[indexNumber]; //gets current Question
+//      const currentQuestionAnswer = currentQuestion.correctOption; //gets current Question's answer
+//      const options = document.getElementsByName('option'); //gets all elements in dom with name of 'option' (in this the radio inputs)
+//      let correctOption = null;
+
+//      options.forEach((option) => {
+//           if (option.value === currentQuestionAnswer) {
+//                //get's correct's radio input with correct answer
+//                correctOption = option.labels[0].id;
+//           }
+//      });
+
+//      //checking to make sure a radio input has been checked or an option being chosen
+//      if (
+//           options[0].checked === false &&
+//           options[1].checked === false &&
+//           options[2].checked === false &&
+//           options[3].checked == false
+//      ) {
+//           document.getElementById('option-modal').style.display = 'flex';
+//      }
+
+//      //checking if checked radio button is same as answer
+//      options.forEach((option) => {
+//           if (
+//                option.checked === true &&
+//                option.value === currentQuestionAnswer
+//           ) {
+//                document.getElementById(correctOption).style.backgroundColor =
+//                     'green';
+//                playerScore++; //adding to player's score
+//                indexNumber++; //adding 1 to index so has to display next question..
+//                //set to delay question number till when next question loads
+//                setTimeout(() => {
+//                     questionNumber++;
+//                }, 1000);
+//           } else if (option.checked && option.value !== currentQuestionAnswer) {
+//                const wrongLabelId = option.labels[0].id;
+//                document.getElementById(wrongLabelId).style.backgroundColor =
+//                     'red';
+//                document.getElementById(correctOption).style.backgroundColor =
+//                     'green';
+//                wrongAttempt++; //adds 1 to wrong attempts
+//                indexNumber++;
+//                //set to delay question number till when next question loads
+//                setTimeout(() => {
+//                     questionNumber++;
+//                }, 1000);
+//           }
+//      });
+// }
