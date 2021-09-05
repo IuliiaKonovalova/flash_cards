513d6b5990bb36ee24fcc70fa6c7da17d8469ea6
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-09-05 14:35:16 +0800

    Add styling to the displated correct and incorrect answers

--- assets/js/grammar_quiz.js
+++ assets/js/grammar_quiz.js
@@ -1,8 +1,6 @@
 /// << << GRAMMAR QUIZ >> >> ///
 
 const cardNextButton = document.querySelector('#card__next');
-const answerOptions = document.querySelectorAll('.grammar-quiz__answer');
-console.log(answerOptions);
 
 // Wait for the DOM to finish loading before running the quiz
 
@@ -80,8 +78,6 @@ const grammarQuestions = [
      },
 ];
 
-// let shuffledQuestions = []; //empty array to hold shuffled selected questions out of all available questions
-
 // function handleQuestions() {
 //      //function to shuffle and push 10 questions to shuffledQuestions array
 //      //app would be dealing with 10questions per session
@@ -95,19 +91,6 @@ const grammarQuestions = [
 //      }
 // }
 
-// let questionNumber = 1; //holds the current question number
-// let playerScore = 0; //holds the player score
-// let wrongAttempt = 0; //amount of wrong answers picked by player
-// let indexNumber = 0; //will be used in displaying next question
-
-// function NextQuestion(index) {
-//      handleQuestions();
-//      const currentQuestion = shuffledQuestions[index];
-//      document.getElementById('grammar-quiz__score').innerHTML = playerScore;
-//      document.getElementById('grammar-quiz__question-number').innerHTML =
-//           currentQuestion.question;
-// }
-
 /**
  *
  * Generate random index for the Quiz
@@ -117,11 +100,17 @@ const createRandomBlock = function () {
      return Math.floor(Math.random() * grammarQuestions.length);
 };
 
+let currentAnswer;
+let shuffledQuestions = [];
+
+let inde;
+
+delete grammarQuestions[inde];
 /**
  * Function generates the Flash Card for the user
  * with all visible elements
  */
-let currentAnswer;
+
 const generateGrammarQuestion = function () {
      let grammarQuestion = document.getElementsByClassName(
           'grammar-quiz__question'
@@ -139,7 +128,12 @@ const generateGrammarQuestion = function () {
      //Applying random question function
 
      let randomBlockIndex = createRandomBlock();
+     shuffledQuestions.push(grammarQuestions[randomBlockIndex]);
+
      currentAnswer = grammarQuestions[randomBlockIndex].correct;
+     // inde = grammarQuestions[randomBlockIndex];
+
+     // console.log(inde);
 
      //Add data to the html from the grammarQuestions array
 
@@ -147,13 +141,11 @@ const generateGrammarQuestion = function () {
           grammarQuestions[randomBlockIndex].grammarQuestion;
 
      for (let i = 0; i < grammarAnswers.length; i++) {
+          grammarAnswers[i].style.backgroundColor = '#00e3c57a';
+          grammarAnswers[i].style.cursor = 'pointer';
           grammarAnswers[i].innerText =
                grammarQuestions[randomBlockIndex].grammarChoices[i];
      }
-     function resetBackgroundAnswers(event) {
-          event.target.style.backgroundColor = '#00e3c57a';
-     }
-     resetBackgroundAnswers();
 };
 
 let answers = document.getElementsByClassName('grammar-quiz__answer');
@@ -162,85 +154,14 @@ for (let answer of answers) {
      answer.addEventListener('click', checkAnswers);
 }
 
+/**
+ * Checking whether answer is correct
+ * */
+
 function checkAnswers(event) {
      if (event.target.innerText === currentAnswer) {
-          event.target.style.backgroundColor = 'green';
-          setTimeout(function () {
-               event.target.style.backgroundColor = '#00e3c57a';
-          }, 500);
+          event.target.style.backgroundColor = '#008a5a';
      } else {
-          event.target.style.backgroundColor = 'red';
+          event.target.style.background = '#c26ee3';
      }
 }
-
-// function checkForAnswers() {
-//      if (
-//           document.querySelectorAll('.grammar-quiz__answer').innerText ===
-//           grammarQuestions.correct
-//      ) {
-//           document.getElementById(
-//                'grammar-quiz__answer1'
-//           ).style.backgroundColor = 'red';
-//           document.getElementById(
-//                'grammar-quiz__answer2'
-//           ).style.backgroundColor = 'red';
-//           document.getElementById(
-//                'grammar-quiz__answer3'
-//           ).style.backgroundColor = 'red';
-//           document.getElementById(
-//                'grammar-quiz__answer4'
-//           ).style.backgroundColor = 'red';
-//      }
-// }
-
-//      const currentQuestion = shuffledQuestions[indexNumber]; //gets current Question
-//      const currentQuestionAnswer = currentQuestion.correctOption; //gets current Question's answer
-//      const options = document.getElementsByName('option'); //gets all elements in dom with name of 'option' (in this the radio inputs)
-//      let correctOption = null;
-
-//      options.forEach((option) => {
-//           if (option.value === currentQuestionAnswer) {
-//                //get's correct's radio input with correct answer
-//                correctOption = option.labels[0].id;
-//           }
-//      });
-
-//      //checking to make sure a radio input has been checked or an option being chosen
-//      if (
-//           options[0].checked === false &&
-//           options[1].checked === false &&
-//           options[2].checked === false &&
-//           options[3].checked == false
-//      ) {
-//           document.getElementById('option-modal').style.display = 'flex';
-//      }
-
-//      //checking if checked radio button is same as answer
-//      options.forEach((option) => {
-//           if (
-//                option.checked === true &&
-//                option.value === currentQuestionAnswer
-//           ) {
-//                document.getElementById(correctOption).style.backgroundColor =
-//                     'green';
-//                playerScore++; //adding to player's score
-//                indexNumber++; //adding 1 to index so has to display next question..
-//                //set to delay question number till when next question loads
-//                setTimeout(() => {
-//                     questionNumber++;
-//                }, 1000);
-//           } else if (option.checked && option.value !== currentQuestionAnswer) {
-//                const wrongLabelId = option.labels[0].id;
-//                document.getElementById(wrongLabelId).style.backgroundColor =
-//                     'red';
-//                document.getElementById(correctOption).style.backgroundColor =
-//                     'green';
-//                wrongAttempt++; //adds 1 to wrong attempts
-//                indexNumber++;
-//                //set to delay question number till when next question loads
-//                setTimeout(() => {
-//                     questionNumber++;
-//                }, 1000);
-//           }
-//      });
-// }
