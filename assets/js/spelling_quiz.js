ea7c01279b3f5f726419a70b84130a7ae0e7f606
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-10-17 00:47:43 -0500

    Resturcture: set up code to egt fetch result

--- assets/js/spelling_quiz.js
+++ assets/js/spelling_quiz.js
@@ -1,36 +1,5 @@
 /* jshint esversion:8, expr:true */
 
-const wordsArray = ['apple', 'banana', 'grapes', 'orange', 'peach', 'pear', 'strawberry', 'watermelon', 'melon', 'raspberries', 'pomegranate', 'plum', 'pineapple', 'persimmon', 'passion fruit', 'papaya', 'mangosteen', 'lime', 'mango', 'grapefruit', 'kiwi', 'lemon', 'lychee', 'mandarin', 'jackfruit', 'durian', 'fruits', 'dragon fruit', 'coconut',
-  'cherry', 'blueberries', 'pumpkin', 'blackberries', 'avocado', 'apricot', 'berries',
-  'color', 'blue', 'brown', 'gold', 'gray', 'green', 'pink', 'purple', 'white', 'red', 'silver', 'yellow',
-  'cat', 'alligator', 'bear', 'butterfly', 'camel', 'dolphin', 'donkey', 'frog', 'giraffe', 'goat', 'gorilla', 'hedgehog', 'hen', 'kangaroo', 'koala', 'owl', 'parrot', 'peacock', 'penguin', 'pig', 'pigeon', 'polar bear', 'puppy', 'raccoon', 'rhino', 'rhinoceros', 'rooster', 'seal', 'sheep', 'squirrel', 'swan', 'whale', 'wolf', 'dog', 'cow', 'kitten',
-  'mouse', 'tiger', 'fish', 'rabbit', 'snake', 'lion', 'deer', 'fox', 'zebra', 'ox', 'bee', 'elephant', 'turtle', 'monkey', 'duck', 'bird', 'panda', 'animals', 'horse', 'sweater', 'blouse', 'boots', 'crocs', 'dress', 'flip-flops', 'hat', 'mittens', 'overalls', 'shorts', 'slippers', 'suit', 'T-shirt', 'sneakers', 'cap', 'coat', 'tie', 'shoe', 'shoes',
-  'jacket', 'jeans', 'pants', 'scarf', 'shirt', 'skirt', 'socks', 'vest', 'uniform', 'gloves',
-  'summer', 'winter', 'spring', 'fall', 'autumn',
-  'China', 'Canada', 'America', 'Britain', 'Japan',
-  'sweet potatoes', 'cookies', 'biscuits', 'bread', 'cherry pie', 'roast chicken', 'chocolate', 'cookbook', 'fish', 'egg', 'hot dog', 'ice cream', 'jam', 'noodles', 'nuts', 'oil', 'pizza', 'rice', 'tofu', 'bacon', 'baguettes', 'barbecue', 'butter', 'cake', 'candies', 'cereal', 'cheese', 'chicken nuggets',
-  'chicken wings', 'chips', 'cottage cheese', 'crab', 'cupcake', 'donuts', 'fried chicken', 'fried egg', 'fried fish', 'hamburger', 'honey', 'lollipops', 'muffin', 'onion rings', 'pancakes', 'pasta', 'popcorn', 'porridge', 'pretzel', 'salad', 'sandwich', 'sausages', 'seafood', 'shrimps', 'soup', 'spices', 'steak', 'taco', 'waffles',
-  'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday',
-  'April', 'August', 'January', 'July', 'June', 'February', 'December', 'March', 'May', 'November', 'October', 'September',
-  'apple juice', 'orange juice', 'coffee', 'tea', 'coke', 'juice', 'lemonade', 'milk', 'milkshake', 'soda', 'soy milk', 'water', 'yogurt',
-  'bag', 'blackboard', 'book', 'eraser', 'brush', 'calculator', 'chair', 'chalk', 'clips', 'clock', 'colored pencils', 'computer', 'crayons', 'desk', 'dictionary', 'door', 'flag', 'folder', 'globe', 'glue', 'highlighter', 'locker', 'lunch box', 'map', 'marker', 'notebooks', 'pen', 'pencil', 'pencil case', 'projector',
-  'ruler', 'scissors', 'school', 'sharpener', 'stapler', 'stickers', 'tablet', 'tape', 'trash can',
-  'cabbage', 'radish', 'chili pepper', 'bell pepper', 'corn', 'onion', 'green beans', 'peas', 'artichoke', 'asparagus', 'turnips', 'spinach', 'zucchinis', 'lady fingers', 'coriander', 'mint', 'mushrooms', 'vegetables', 'celery', 'garlic', 'ginger', 'green onion', 'cauliflower', 'leek', 'lettuce', 'Brussels sprouts', 'broccoli', 'beet', 'eggplant', 'sweet potatoes', 'tomato', 'potato', 'carrot', 'cucumber',
-  'race', 'relay race', 'go fishing', 'go roller-skating', 'go skiing', 'go swimming', 'go climbing', 'go hiking', 'basketball', 'tennis ball', 'football', 'ping-pong ball', 'golf ball', 'baseball',
-  'flute', 'fiddle', 'accordion', 'piano', 'violin',
-  'cloud', 'flower', 'grass', 'lake', 'leaf', 'leaves', 'moon', 'river', 'stars', 'sun', 'tree', 'branch', 'bush', 'cactus', 'cave', 'cliff', 'desert', 'earth', 'field', 'fire', 'forest', 'garden', 'hill', 'iceberg', 'icicles', 'island', 'jungle', 'lightning', 'meadow', 'moss', 'mountain', 'mud', 'mushrooms', 'oasis', 'ocean', 'path', 'planet', 'plant', 'ice', 'pond', 'rain', 'rainbow', 'rock', 'root', 'rose', 'sand', 'sea', 'seed', 'sky', 'smoke', 'snow', 'snowflake', 'soil', 'stones', 'volcano', 'waterfall', 'water', 'wave',
-  'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty',
-  'airport', 'amusement park', 'apartment building', 'aquarium', 'underwater zoo', 'bank', 'beach', 'bookshop', 'bookstore', 'bridge', 'bus stop', 'circus', 'cinema', 'classroom', 'gate', 'hospital', 'house', 'hut', 'library', 'museum', 'park', 'police station', 'post office', 'restaurant', 'school', 'shopping mall', 'stadium', 'supermarket', 'theater', 'train station', 'zoo', 'atm', 'castle', 'church', 'crossing', 'crosswalk', 'factory', 'farm', 'fire station', 'gym', 'hotel', 'market', 'gas station', 'petrol station', 'pharmacy', 'road', 'sidewalk', 'pavement', 'street', 'swimming pool',
-  'actor', 'actress', 'architect', 'businessman', 'businesswoman', 'artist', 'astronaut', 'baker', 'barber', 'builder', 'butcher', 'cameraman', 'carpenter', 'cashier', 'coach', 'cook', 'dancer', 'delivery man', 'cleaner', 'dentist', 'doctor', 'driver', 'farmer', 'firefighter', 'fisherman', 'florist', 'hairdresser', 'musician', 'nurse', 'pharmacist', 'pilot', 'player', 'policeman', 'policewoman', 'programmer', 'scientist', 'singer', 'soldier', 'sportsman', 'swimmer', 'tailor', 'teacher', 'vet', 'veterinarian', 'waiter', 'waitress', 'worker', 'writer',
-  'ambulance', 'bike', 'boat', 'bus', 'car', 'fire engine', 'helicopter', 'hot-air balloon', 'motorbike', 'on foot', 'plane', 'airplane', 'rocket', 'school bus', 'scooter', 'ship', 'skateboard', 'subway', 'taxi', 'tractor', 'train', 'truck', 'UFO', 'van',
-  'lentil', 'beans', 'buckwheat', 'oats', 'rice', 'soybeans', 'wheat', 'circle', 'cross', 'rectangle', 'square', 'star', 'triangle',
-  'arm', 'back', 'beard', 'cheek', 'chin', 'ear', 'elbow', 'eye', 'eyebrow', 'eyelashes', 'face', 'feet', 'finger', 'foot', 'forehead', 'freckles', 'hair', 'hand', 'head', 'knee', 'leg', 'lip', 'mole', 'mouth', 'mustache', 'nail', 'neck', 'nose', 'shoulder', 'tail', 'teeth', 'toes', 'tongue', 'tooth', 'tummy', 'wrinkles', 'wrist',
-  'come', 'dance', 'drink', 'eat', 'fly', 'give', 'jump', 'learn', 'listen', 'look', 'plant', 'play', 'run', 'see', 'sing', 'sit', 'study', 'swim', 'talk', 'wait', 'wake up', 'walk', 'watch', 'work', 'write',
-  'cloudy', 'cold', 'dry', 'foggy', 'hot', 'rainstorm', 'rainy', 'sandstorm', 'snowstorm', 'snowy', 'sunny', 'warm', 'wet', 'windstorm', 'windy',
-  'mother', 'father', 'grandmother', 'grandfather', 'sister', 'brother', 'baby', 'child', 'children',
-];
-
-
 //main variables
 const btnCheck = document.querySelector('#btn__check');
 const btnNextCard = document.querySelector('#card__next');
@@ -38,6 +7,7 @@ const btnVoiceUs = document.querySelector("#voice__option--US");
 const btnVoiceGb = document.querySelector("#voice__option--GB");
 let currentWord;
 let temporaryWordsArray = [];
+let wordsArray
 
 const modal = document.querySelector('#modal');
 const overlay = document.querySelector('#overlay');
@@ -47,9 +17,9 @@ const btnOpenModal = document.querySelector('#button__instructions');
 // Wait for the DOM to finish loading before running the quiz
 
 document.addEventListener('DOMContentLoaded', function () {
-
+  fetchWordsData();
   // New word;
-  generateWord();
+
 
 
   // Listen for the arrow right to switch the question:
@@ -92,6 +62,31 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 /**
+ * Fetch data from the JSON file
+ */
+async function fetchWordsData() {
+  try {
+    const res = await fetch("assets/js/json/words-data.json");
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    const data = await res.json();
+    if (Array.isArray(data.wordsArray)) {
+      console.log(data.wordsArray);
+      wordsArray = data.wordsArray;
+      generateWord();
+      return wordsArray;
+    } else {
+      throw new Error("Data does not contain an array of words");
+    }
+  } catch (error) {
+    console.error("Error fetching words data:", error);
+    return [];
+  }
+}
+
+
+/**
  * Open instructions modal window
  */
 const openModal = function () {
@@ -111,10 +106,15 @@ const closeModal = function () {
  * Add voice task on click American or British accents
  */
 function speakFunction(lang) {
-  let msg = new SpeechSynthesisUtterance();
-  msg.lang = `en-${lang}`;
-  msg.text = currentWord.replace('/', 'or');
-  speechSynthesis.speak(msg);
+  console.log(currentWord);
+  if (typeof currentWord === 'string') {
+    let msg = new SpeechSynthesisUtterance();
+    msg.lang = `en-${lang}`;
+    msg.text = currentWord.replace('/', 'or');
+    speechSynthesis.speak(msg);
+  } else {
+    console.error("currentWord is not a string.");
+  }
 }
 
 /**
@@ -122,6 +122,7 @@ function speakFunction(lang) {
  * Generate random index for the words array
  */
 const createRandom = function () {
+  console.log(wordsArray.length);
   return Math.floor(Math.random() * wordsArray.length);
 };
 
@@ -141,8 +142,10 @@ const generateWord = function () {
   answerInput.value = '';
 
   const randomIndex = createRandom();
+  console.log(randomIndex);
 
   currentWord = wordsArray[randomIndex];
+  console.log(currentWord);
 
   //Delete shown word from the main array
   //Push the word into temporary array
